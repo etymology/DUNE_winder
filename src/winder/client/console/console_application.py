e8e8a579939f1021ff40878b4587093837babc79
Chris Herrick <cherrick@bb7.com>
2016-03-16 11:51:59 -0500

    The kv file seems to be causing problems, so it is not being used.

--- src/winder/client/console/console_application.py
+++ src/winder/client/console/console_application.py
@@ -1,12 +1,38 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+
+# from .application_shared import AppShare
 from .ui.kivy_mixins import BackgroundColorMixin
+from .ui.manual_movement import ManualMovementControl
+from .ui.operation_bar import OperationBar
 
-Builder.load_file( r"console_application.kv" )
+class RootWidget( BackgroundColorMixin, BoxLayout ):
+   def __init__( self, **kwargs ):
+      super( RootWidget, self ).__init__( orientation = "vertical", **kwargs )
+      self._construct()
 
-class RootWidget( BackgroundColorMixin, FloatLayout ):
-   pass
+   def _construct( self, **kwargs ):
+#       self.bg_color = AppShare.instance().settings.theme.control_color_value
+
+      self.operation_bar = OperationBar( size_hint = ( 1, .25 ) )
+      self.tabbed_layout = self._construct_tabbed_layout()
+
+      for widget in [self.operation_bar, self.tabbed_layout]:
+         self.add_widget( widget )
+
+   def _construct_tabbed_layout( self, **kwargs ):
+      self.main_tab = TabbedPanelItem( text = "Main", **kwargs )
+      self.apa_tab = TabbedPanelItem( text = "APA", **kwargs )
+      self.manual_movement_control = ManualMovementControl()
+      self.manual_movement_tab = TabbedPanelItem( text = "Manual", content = self.manual_movement_control, **kwargs )
+
+      result = TabbedPanel( do_default_tab = False, **kwargs )
+
+      for widget in [ self.main_tab, self.apa_tab, self.manual_movement_tab ]:
+         result.add_widget( widget )
+
+      return result
 
 class ConsoleClientApplication( App ):
    def build( self ):
