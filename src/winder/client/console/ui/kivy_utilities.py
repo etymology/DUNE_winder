d0a600f3ff7e79a7c48cec0716fd91490b9b917b
Chris Herrick <cherrick@bb7.com>
2016-04-11 12:22:27 -0500

    Added color retrieval method.

--- src/winder/client/console/ui/kivy_utilities.py
+++ src/winder/client/console/ui/kivy_utilities.py
@@ -1,3 +1,5 @@
+from kivy.graphics import Color
+
 class KivyUtilities( object ):
    @staticmethod
    def add_children_to_widget( widget, children = None, *args ):
@@ -10,3 +12,16 @@ class KivyUtilities( object ):
 
       for child in new_children:
          widget.add_widget( child )
+
+   @staticmethod
+   def get_color_from_value( value ):
+      color_component_count = len( value )
+
+      if color_component_count == 3:
+         result = Color( value[ 0 ], value[ 1 ], value[ 2 ] )
+      elif color_component_count == 4:
+         result = Color( value[ 0 ], value[ 1 ], value[ 2 ], value[ 3 ] )
+      else:
+         raise Exception( "Invalid color: {}".format( str( value ) ) )
+
+      return result
