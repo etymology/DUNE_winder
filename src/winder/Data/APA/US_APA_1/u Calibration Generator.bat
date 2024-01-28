547a3add730a04db2b70d9288c7e91c753954d73
ben <benjamin.oye@gmail.com>
2024-01-28 15:53:33 -0600

    oye commit

---
+++ src/winder/Data/APA/US_APA_1/u Calibration Generator.bat
@@ -0,0 +1,2 @@
+cd C:\bin\T01-Winder\src\winder\Data\APA\ProtoDUNE-1
+python csv2calibration.py U uCalibration.csv U_Calibration.xml
---
+++ src/winder/Data/APA/US_APA_1/v Calibration Generator.bat
@@ -0,0 +1,3 @@
+call C:\ProgramData\anaconda3\Scripts\activate.bat C:\ProgramData\anaconda3\envs\DunePy27
+cd 'C:\DUNE\dune_winder\src\winder\Data\APA\Wood APA'
+python csv2calibration.py V vCalibration.csv V_Calibration.xml
