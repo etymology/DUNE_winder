2fd365de43f5cdc70c89c59891bb03d5257ec009
DUNE_admin <benjamin.oye@gmail.com>
2024-04-03 11:30:12 -0500

    settings fixed

--- src/winder/Control/Machine/Settings.py
+++ src/winder/Control/Machine/Settings.py
@@ -7,79 +7,86 @@
 #   Benjamin Oye <oye@uchicago.edu> [port to python3, Jan 2024]
 ###############################################################################
 import platform
+from pathlib import Path
 
 class Settings :
 
-  SERVER_PORT                 = 6626  # Default TCP port number (plank's constant).
-  WEB_SERVER_PORT             = 80    # Port for web server (80 is default).
-  SERVER_MAX_DATA_SIZE        = 1024  # Max data that can be read from server at once.
-  SERVER_BACK_LOG             = 5     # Default recommended by Python manual.
-  CLIENT_MAX_DATA_SIZE        = 1024  # Max data that can be read from client at once.
-  IO_UPDATE_TIME              = 0.1   # In seconds.  Currently 10 times/sec.
+    SERVER_PORT                 = 6626  # Default TCP port number (plank's constant).
+    WEB_SERVER_PORT             = 80    # Port for web server (80 is default).
+    SERVER_MAX_DATA_SIZE        = 1024  # Max data that can be read from server at once.
+    SERVER_BACK_LOG             = 5     # Default recommended by Python manual.
+    CLIENT_MAX_DATA_SIZE        = 1024  # Max data that can be read from client at once.
+    IO_UPDATE_TIME              = 0.1   # In seconds.  Currently 10 times/sec.
 
-  # Path to configuration file.
-  # Check the operating system
-  if platform.system() == "Windows":
-      CONFIG_FILE = "C:/Users/Dune Admin/winder_py2/src/winder/configuration.xml"
-      UI_VERSION_FILE = "C:/Users/Dune Admin/winder_py2/src/winder/WebUI/version.xml"
-      WEB_DIRECTORY = "C:/Users/Dune Admin/winder_py2/dune_winder2/src/winder\WebUI"
-  elif platform.system() == "Linux":
-      CONFIG_FILE = "../configuration.xml"
-      UI_VERSION_FILE = "../dune_winder/src/winder/WebUI/version.xml"
-      WEB_DIRECTORY = "/home/ben/DUNE/dune_winder/src/winder/WebUI"
-  else:
-      # Handle other operating systems if needed
-      print("Unsupported operating system")
+    src_winder = Path(__file__).parents[2]
+    # Path to configuration file.
+    # Check the operating system
 
-  # Path and name of version information file.
-  VERSION_FILE = "C:\\Users\\Dune Admin\\winder_py2\\dune_winder2\\src\\winder\\Control\\version.xml"
+    CONFIG_FILE = str(src_winder / "configuration.xml")
+    UI_VERSION_FILE = str(src_winder / "WebUI/version.xml")
+    WEB_DIRECTORY = str(src_winder / "WebUI")
+    VERSION_FILE = str(src_winder / "Control/version.xml")
+
+    # if platform.system() == "Windows":
+    #     CONFIG_FILE = str(src_winder / "configuration.xml")
+    #     UI_VERSION_FILE = str(src_winder / "WebUI/version.xml")
+    #     WEB_DIRECTORY = str(src_winder / "WebUI")
+    #     VERSION_FILE = str(src_winder / "Control/version.xml")
+
+    # elif platform.system() == "Linux":
+    #     CONFIG_FILE = str(src_winder / "configuration.xml")
+    #     UI_VERSION_FILE = str(src_winder / "WebUI/version.xml")
+    #     WEB_DIRECTORY = str(src_winder / "WebUI")
+    #     VERSION_FILE = str(src_winder / "Control/version.xml")
+    # else:
+    #     # Handle other operating systems if needed
+    #     print("Unsupported operating system")
+
+    G_CODE_LOG_FILE = "_gCode.gc"
+
+    IO_LOG = str(src_winder / "Data/IO_log.csv")
+
+    MACHINE_CALIBRATION_FILE = "machineCalibration.xml"
 
 
-  G_CODE_LOG_FILE = "_gCode.gc"
 
-  IO_LOG = "../Data/IO_log.csv"
+    # File making up the version for the control software.
+    CONTROL_FILES = ".*/.py$"
 
-  MACHINE_CALIBRATION_FILE = "machineCalibration.xml"
+    # File making up the version for the user interface.
+    UI_FILES = ".*/.html$|.*/.css$|.*/.js$"
 
+    #---------------------------------------------------------------------
+    @staticmethod
+    def defaultConfig( configuration ) :
+        """
+        Setup default values for configuration.
+        """
+        configuration.default( "plcAddress", "192.168.140.13" )
 
+        # Location of camera's last captured image.
+        configuration.default( "cameraURL", "ftp://admin@192.168.140.19/image.bmp" )
+        configuration.default( "pixelsPer_mm", 18 )
 
-  # File making up the version for the control software.
-  CONTROL_FILES = ".*\.py$"
+        # Default for GUI server.
+        configuration.default( "serverAddress", "127.0.0.1" )
+        configuration.default( "serverPort", Settings.SERVER_PORT )
 
-  # File making up the version for the user interface.
-  UI_FILES = ".*\.html$|.*\.css$|.*\.js$"
+        # Default for GUI server.
+        configuration.default( "webServerPort", 80 )
 
-  #---------------------------------------------------------------------
-  @staticmethod
-  def defaultConfig( configuration ) :
-    """
-    Setup default values for configuration.
-    """
-    configuration.default( "plcAddress", "192.168.140.13" )
+        # Log file locations.
+        configuration.default( "LogDirectory", "../Data" )
+        configuration.default( "machineCalibrationPath", "../Data/" )
+        configuration.default( "machineCalibrationFile", Settings.MACHINE_CALIBRATION_FILE )
+        configuration.default( "APA_LogDirectory", "../Data/APA" )
+        configuration.default( "recipeDirectory", "../Recipes" )
+        configuration.default( "recipeArchiveDirectory", "../Recipes" )
 
-    # Location of camera's last captured image.
-    configuration.default( "cameraURL", "ftp://admin@192.168.140.19/image.bmp" )
-    configuration.default( "pixelsPer_mm", 18 )
+        # Velocity limits.
+        configuration.default( "maxVelocity", 16 * 25.4 ) # 16 inches/second
+        configuration.default( "maxSlowVelocity", 1 * 25.4 ) # 1 inches/second
 
-    # Default for GUI server.
-    configuration.default( "serverAddress", "127.0.0.1" )
-    configuration.default( "serverPort", Settings.SERVER_PORT )
-
-    # Default for GUI server.
-    configuration.default( "webServerPort", 80 )
-
-    # Log file locations.
-    configuration.default( "LogDirectory", "../Data" )
-    configuration.default( "machineCalibrationPath", "../Data/" )
-    configuration.default( "machineCalibrationFile", Settings.MACHINE_CALIBRATION_FILE )
-    configuration.default( "APA_LogDirectory", "../Data/APA" )
-    configuration.default( "recipeDirectory", "../Recipes" )
-    configuration.default( "recipeArchiveDirectory", "../Recipes" )
-
-    # Velocity limits.
-    configuration.default( "maxVelocity", 16 * 25.4 ) # 16 inches/second
-    configuration.default( "maxSlowVelocity", 1 * 25.4 ) # 1 inches/second
-
-    # Acceleration limits.
-    configuration.default( "maxAcceleration", 8 * 25.4 ) # 8 inches/s^2
-    configuration.default( "maxDeceleration", 2 * 25.4 ) # 2 inches/s^2
+        # Acceleration limits.
+        configuration.default( "maxAcceleration", 8 * 25.4 ) # 8 inches/s^2
+        configuration.default( "maxDeceleration", 2 * 25.4 ) # 2 inches/s^2
